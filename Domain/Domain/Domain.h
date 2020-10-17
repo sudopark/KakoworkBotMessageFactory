cd64adc2cd517cc9826b6feb60787422524471e8
sudo.park <sudo.park@kakaocorp.com>
2020-10-17 14:43:37 +0900

    Domain framework 추가

---
+++ Domain/Domain/Domain.h
@@ -0,0 +1,18 @@
+//
+//  Domain.h
+//  Domain
+//
+//  Created by Sudo.park on 2020/10/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Domain.
+FOUNDATION_EXPORT double DomainVersionNumber;
+
+//! Project version string for Domain.
+FOUNDATION_EXPORT const unsigned char DomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Domain/PublicHeader.h>
+
+
